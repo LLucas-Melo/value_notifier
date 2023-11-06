a3e3020e485f209c9ac52f45cf17a9238dd0ebda
lucas.melo <lukas_augusto@live.com>
2023-11-06 09:23:37 -0300

    injeção de dependecia com provider

--- lib/products/product_page.dart
+++ lib/products/product_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:uno/uno.dart';
 import 'package:value_notifier/products/services/products_service.dart';
 import 'package:value_notifier/products/state/product_state.dart';
@@ -12,57 +13,46 @@ class ProductPage extends StatefulWidget {
 }
 
 class _ProductPageState extends State<ProductPage> {
-  final store = ProductStore(ProductService(Uno()));
-
   @override
   void initState() {
     super.initState();
-    store.fetchProducts();
+    context.read<ProductStore>().fetchProducts();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Product Page'),
-      ),
-      body: ValueListenableBuilder(
-        valueListenable: store,
-        builder: (_, state, child) {
-          print(state);
+    final store = context.watch<ProductStore>();
+    final state = store.value;
+    Widget? child;
 
-          if (state is LoadingProductState) {
-            print('oi carregando');
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (state is ErrorProductState) {
-            print('oi com erro');
-            return Center(
-              child: Text(state.menssagem),
-            );
-          }
-          if (state is SuccessProductState) {
-            print(state.products.length);
-            return ListView.builder(
-              itemCount: state.products.length,
-              itemBuilder: (context, index) {
-                final product = state.products[index];
-                print(product.title);
-                return ListTile(
-                  title: Text(product.title),
-                );
-              },
-            );
-          }
-          return Container(
-            width: 50,
-            height: 50,
-            color: Colors.amber,
+    if (state is LoadingProductState) {
+      child = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    if (state is ErrorProductState) {
+      print('oi com erro');
+      child = Center(
+        child: Text(state.menssagem),
+      );
+    }
+    if (state is SuccessProductState) {
+      child = ListView.builder(
+        itemCount: state.products.length,
+        itemBuilder: (context, index) {
+          final product = state.products[index];
+          print(product.title);
+          return ListTile(
+            title: Text(product.title),
           );
         },
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Page'),
       ),
+      body: child ?? Container(),
     );
   }
 }
--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:uno/uno.dart';
 import 'package:value_notifier/products/product_page.dart';
+import 'package:value_notifier/products/services/products_service.dart';
+import 'package:value_notifier/products/store/product_store.dart';
 
 class AppWiget extends StatelessWidget {
   const AppWiget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: ProductPage(),
+    return MultiProvider(
+      providers: [
+        Provider(create: (_) => Uno()),
+        Provider(create: (context) => ProductService(context.read())),
+        ChangeNotifierProvider(
+          create: (context) => ProductStore(
+            ProductService(
+              context.read(),
+            ),
+          ),
+        )
+      ],
+      child: const MaterialApp(
+        home: Scaffold(
+          body: ProductPage(),
+        ),
       ),
     );
   }
